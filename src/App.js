b0d14cf37029718ae9e9d806404c6096a000c816
Gabriel Oliveira <patonoideoriginal@gmail.com>
2022-03-26 01:32:27 -0300

    adiciona configuração para o firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import AgencyLandingPage from "pages/AgencyLandingPage.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = require("./firebase_config.json");
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
